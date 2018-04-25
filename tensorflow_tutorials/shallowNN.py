99ee113ef0844eff5c6072e15df29102a9969ca8
NoviScl <sichenglei1125@gmail.com>
2018-04-25 19:47:40 +0800

    visualization

---
+++ tensorflow_tutorials/shallowNN.py
@@ -0,0 +1,40 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+import numpy as np
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    bias = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b=tf.matmul(inputs, Weights) + bias
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis] #set to (300,1) dimension
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+
+layer1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+prediction = add_layer(layer1, 10, 1, activation_function=None)
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), axis=1))
+
+train_step = tf.train.AdamOptimizer(0.1).minimize(loss)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i%50 == 0:
+        print (sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+
